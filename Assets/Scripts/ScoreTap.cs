e6f4e52c01fbcf7fb6f768b64c3fbe373209fd8f
Motherl0de <timmikvo@gmail.com>
2025-03-13 08:55:13 +0200

    Added new sprites and create change sprite

--- Assets/Scripts/ScoreTap.cs
+++ Assets/Scripts/ScoreTap.cs
@@ -1,19 +1,29 @@
-using System.Collections;
-using System.Collections.Generic;
+
 using TMPro;
 using UnityEngine;
+using UnityEngine.UI;
 
 namespace TapGame
 {
     public class ScoreTap : MonoBehaviour
     {
         [SerializeField] private TMP_Text _scoreText;
+        [SerializeField] private Sprite _image;
         private int _score = 0;
+        [SerializeField]private Button _button;
 
         public void AddScore()
         {
             _score += 1;
             _scoreText.text = "Score: " + _score;
+        }
+
+        private void Update()
+        {
+            if (_score > 10)
+            {
+                _button.image.sprite = _image;
+            }
         }
     }
 }
