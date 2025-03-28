541338e5296998dd2676101c9acea8710e0e50b1
Motherl0de <timmikvo@gmail.com>
2025-03-28 11:36:15 +0200

    Fix delete obj

---
+++ Assets/Scripts/DestroyGameObject.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+
+namespace TapGame
+{
+    public sealed class DestroyGameObject : MonoBehaviour
+    {
+        private ADS _ads;
+        [SerializeField]private GameObject _target;
+
+        private void OnEnable()
+        {
+            _ads = FindObjectOfType<ADS>();
+            _ads.AdsShown.AddListener(DestroySelf);
+        }
+
+        private void DestroySelf()
+        {
+            Destroy(_target);
+        }
+
+        private void OnDisable()
+        {
+            _ads.AdsShown.RemoveListener(DestroySelf);
+            _ads = null;
+        }
+    }
+}
